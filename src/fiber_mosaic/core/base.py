aa4cb0aa2a015d47b18b373a9df9a6c048b02263
Arielle Leon <ariellel@alleninstitute.org>
2026-06-30 21:28:04 -0700

    fix: exposing add_segment wrapper

--- src/fiber_mosaic/core/base.py
+++ src/fiber_mosaic/core/base.py
@@ -61,6 +61,10 @@ class BaseFiberPhotometryExtractor(BaseRecording):
         """Return the number of fibers (channels) in this recording."""
         return self.get_num_channels()
 
+    def add_segment(self, segment) -> None:
+        """Attach a contiguous block of fluorescence data to this recording."""
+        super().add_segment(segment)
+
     @classmethod
     def get_streams(cls, *args, **kwargs) -> tuple[list, list]:
         """
